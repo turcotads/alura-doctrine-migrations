d1669a068d2cd92f1e13dab41acd5507bd29f403
Fábio Madeira Peres <fabinhomadeira@gmail.com>
2022-08-07 16:38:40 -0300

    Usando QueryBuilder

--- src/Repository/DoctrineStudentRepository.php
+++ src/Repository/DoctrineStudentRepository.php
@@ -10,13 +10,12 @@ class DoctrineStudentRepository extends EntityRepository {
 	 * @return Student[]
 	 */
 	public function studentsAndCourses(): array {
-		$dql = 'SELECT student, phone, course
-		FROM Alura\\Doctrine\\Entity\\Student student
-		LEFT JOIN student.phones phone
-		LEFT JOIN student.courses course';
-
-		return $this->getEntityManager()
-			->createQuery($dql)
-			->getResult();
+		return $this->createQueryBuilder('student')
+		->addSelect('phone')
+		->addSelect('course')
+		->leftJoin('student.phones', 'phone')
+		->leftJoin('student.courses', 'course')
+		->getQuery()
+		->getResult();
 	}
 }
